b9c56c91a02683baa40fe98062030405c2707ce6
kong-kong-ru-ye <107926687+kong-kong-ru-ye@users.noreply.github.com>
2023-02-15 09:49:27 +0800

    Add files via upload

--- src/huanxiang/YinYong.java
+++ src/huanxiang/YinYong.java
@@ -2,6 +2,7 @@ package huanxiang;
 
 import arc.util.io.Reads;
 import arc.util.io.Writes;
+import huanxiang.core.*;
 import huanxiang.type.*;
 import mindustry.gen.Building;
 
@@ -10,12 +11,16 @@ public class YinYong {
     public static Writes write;
     public static Reads read;
 
-
+//type
     public static AttributeInputheatGenericCrafter AttributeInputheatGenericCrafter;
     public static AttributeInputOutputheatGenericCrafter AttributeInputOutputheatGenericCrafter;
     public static AttributeOutputheatGenericCrafter AttributeOutputheatGenericCrafter;
     public static HiddenItem HiddenItem;
     public static HiddenLiquid HiddenLiquid;
-
+//core
+    public static HuanXiangBuilding HuanXiangBuilding;
+    public static HXBlock HXBlock;
+    public static HXConsumePayloads HXConsumePayloads;
+    public static HXEffect HXEffect;
 
 }
