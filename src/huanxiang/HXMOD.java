b520172c7b019cca828d192039ef3692d7279551
kong-kong-ru-ye <107926687+kong-kong-ru-ye@users.noreply.github.com>
2023-02-18 10:01:53 +0800

    Add files via upload

--- src/huanxiang/HXMOD.java
+++ src/huanxiang/HXMOD.java
@@ -2,11 +2,25 @@ package huanxiang;
 
 import mindustry.mod.Mod;
 
+import static huanxiang.core.HXSpecial.HXModJS.HXMods;
+
 public class HXMOD extends Mod {
-    public HXMOD() {
+
+public static String ModName = "hx";
+   
+    public static String DawnName(String name){
+        return ModName + "-" + name;
+    }
+     
+      
+        public HXMOD() {
     }
     
     @Override
     public void loadContent() {
+         HXMods();
+    
+    
+   
     }
 }
