2a9f7dbb10288d22700ce0e4ff4a83a7f2840c2d
kong-kong-ru-ye <107926687+kong-kong-ru-ye@users.noreply.github.com>
2023-03-08 19:28:40 +0800

    Add files via upload

---
+++ src/huanxiang/content/fsdgdgdf.java
@@ -0,0 +1,17 @@
+package huanxiang.content;
+
+import mindustry.entities.abilities.SuppressionFieldAbility;
+import mindustry.gen.Mechc;
+import mindustry.gen.Unitc;
+import mindustry.type.UnitType;
+import mindustry.type.ammo.PowerAmmoType;
+
+public class fsdgdgdf {
+    public static UnitType nova, pulsar, quasar;
+
+    public static void load() {
+
+
+
+    }
+}
