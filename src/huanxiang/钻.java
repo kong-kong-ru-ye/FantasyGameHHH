606d6f5ce20475313038b90f8762d557375792ef
kong-kong-ru-ye <107926687+kong-kong-ru-ye@users.noreply.github.com>
2023-03-08 19:13:53 +0800

    Add files via upload

---
+++ src/huanxiang/钻.java
@@ -0,0 +1,16 @@
+package huanxiang;
+
+
+import mindustry.type.UnitType;
+import mindustry.world.blocks.production.BeamDrill;
+
+public class 钻 {
+
+    public static class 墙壁钻 extends BeamDrill {
+
+
+        public 墙壁钻(String name) {
+            super(name);
+        }
+    }
+}
